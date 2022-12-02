92feb2fa61d6d81fea93e33e830b1946f759f6db
Shubham Mali <shubhammali31299@gmail.com>
2022-12-02 16:32:12 +0530

    started basics of functions

---
+++ fortrial.js
@@ -0,0 +1,16 @@
+let city = ["hubli","dharwad","belgavi","mangalore","mumbai"];
+
+console.log(city);
+console.log(city.length);//5
+
+// for (a=0;a<city.length;a++){
+//     console.log(city[a]);
+// }
+
+// console.log(city[0]);
+// console.log(city[1]);
+// console.log(city[2]);
+// console.log(city[3]);
+// console.log(city[4]);
+
+
---
+++ functions.js
@@ -0,0 +1,102 @@
+// functions are like our servants in javascript, they perform tasks which we give them
+
+// function shubhamsBdayy(){
+//     console.log("happiest birthdayy Shubham! Enjoy your Day");
+// }
+// // call or invoke or run a funtion 
+// shubhamsBdayy();
+// shubhamsBdayy();
+// shubhamsBdayy();
+// shubhamsBdayy();
+
+// function add(){
+//     console.log(2+4);
+// }
+
+// add();
+// add();
+// add();
+// add();
+
+
+// function calc(a,b){  // a and b are parameters 
+//     return a+b;
+// }
+// const returnedValue= calc(6,7);  // value given to the parameters are called arguments 
+
+// console.log(returnedValue);
+
+
+
+// Odd or even using functions
+
+// let num = 32;
+
+// function isEven(){
+//      return num % 2; 
+// }
+
+// const value = isEven();
+// // console.log(value)
+
+// if (value==0){
+//     console.log(`${num} is even`)
+// }else{
+//     console.log(`${num} is odd`)
+// }
+
+
+
+// function isEven(num){
+//     if (num % 2 ===0){
+//         return 'Even';
+//     }else{
+//         return 'Odd';
+//     }
+// }
+
+// console.log(isEven(22));
+// console.log(isEven(26));
+// console.log(isEven(28));
+// console.log(isEven(23));
+// console.log(isEven(21));
+// console.log(isEven(29));
+
+
+
+// make a function 
+// input : string 
+// output : first Character
+
+// function input(para){
+//   return  para.slice(0,1);
+// }
+
+// let finalValue = input("Shubham");
+// console.log(`First letter in given string is ${finalValue}`);
+
+
+// second method 
+
+// function firstChar(anyString){
+// return anyString[0];
+// }
+
+// console.log(firstChar("Akshat"));
+
+
+// function 
+// input : array , target (number)
+// output : index of target if target is present in array 
+
+// function input(anyArray, target){
+// for(i=0; i<=anyArray.length; i++){
+//     if(anyArray[i]===target){
+//         return i ;
+//     }
+   
+// } return 'not present';
+// }
+
+// const myArray = [1,2,3,4,5,6,7,8,9];
+// console.log(input (myArray, 8));   
